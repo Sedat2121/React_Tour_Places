0af6e5d0eb7b72c72ce7b231254a9f7d75a87d5d
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-26 18:02:55 +0000

    Card.jsx is created for card data and its linked

--- tour-app/src/components/Main/Main.jsx
+++ tour-app/src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import "./Main.scss";
+import { data } from "../../helper/data";
+import Card from "./Card";
 
 
 const Main = () => {
   return (
     <div className="card-container">
-        <h1>CARD</h1>
+        {data.map((item) => <Card {...item}/>  )}
     </div>
   );
 };

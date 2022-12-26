584c32b01e29f9748452551f245d4f243b8ed3de
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-26 17:01:27 +0000

    Header component is completed and styled.

--- tour-app/src/App.js
+++ tour-app/src/App.js
@@ -1,12 +1,14 @@
 import "./App.scss";
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
 
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 };

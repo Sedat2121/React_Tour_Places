b5c0b0fa7659e0351b16e0e288d026f4390567d2
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-26 16:14:25 +0000

    Navbar componnet is created and styled

--- tour-app/src/App.js
+++ tour-app/src/App.js
@@ -1,12 +1,12 @@
 import "./App.scss";
-
 import React from 'react';
+import Navbar from "./components/Navbar/Navbar";
+
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Welcome to my React tour app.</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores perspiciatis facilis nesciunt provident dignissimos. Et molestias veniam amet placeat nobis tenetur sequi facere eos repudiandae.</p>
+      <Navbar />
     </div>
   );
 };

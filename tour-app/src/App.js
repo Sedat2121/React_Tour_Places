5f1e96721569faef785130e57cdfd517c337083d
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-12-26 00:50:11 +0000

    app.scss is created and linked

--- tour-app/src/App.js
+++ tour-app/src/App.js
@@ -1,24 +1,15 @@
 import "./App.scss";
 
-function App() {
+import React from 'react';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to my React tour app.</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores perspiciatis facilis nesciunt provident dignissimos. Et molestias veniam amet placeat nobis tenetur sequi facere eos repudiandae.</p>
     </div>
   );
-}
+};
 
 export default App;
+ 
